09ab443f0f948eb44ffd8d4fd9fe9a4141d84c06
Maria de los Angeles Ramirez <mariaramireeez2002@gmail.com>
2025-07-15 11:59:57 -0400

    feat: banner cloud

--- src/components/Atoms/BannerImage/index.tsx
+++ src/components/Atoms/BannerImage/index.tsx
@@ -2,5 +2,5 @@
 import Banner from '../../../../public/images/banner-home.png'
 
 export const BannerImage = () => {
-    return <img src={Banner} className={'w-full h-[70vh]'} alt="BannerImage" />
+    return <img src={Banner} className={'w-full h-[75vh]'} alt="BannerImage" />
 }
--- src/components/Molecules/Banner/index.tsx
+++ src/components/Molecules/Banner/index.tsx
@@ -1,6 +1,8 @@
 import type {FC} from "react";
 import {BannerImage} from "../../Atoms/BannerImage";
-
+import AWSImage from '../../../../public/images/aws.png';
+import GoogleCloudImage from '../../../../public/images/google-cloud.png';
+import AzureImage from '../../../../public/images/azure.png';
 export type BannerProps={
     titleOne: string;
     titleTwo: string;
@@ -20,6 +22,21 @@ export const Banner:FC<BannerProps> = ({titleOne,titleTwo,description})=>{
            <h2 className={'lg:text-[60px]  md:text-[40px]   font-bold '}>{titleTwo}</h2>
 
            <p className={'text-lg max-w-[700px] pt-10'}>{description}</p>
+            <h2 className={'text-white'}>gholsa</h2>
+           <div className={'flex flex-wrap items-center pt-6'}>
+               <div className={'px-2'}>
+                   <img className={'lg:h-20 h-14'} src={AWSImage}/>
+
+               </div>
+               <div className={'px-2 pt-2 '}>
+                   <img className={'lg:h-20 lg:w-72 h-14'} src={GoogleCloudImage} alt=""/>
+
+               </div>
+               <div className={'px-2 pt-2'}>
+                   <img className={'lg:h-20 lg:w-64 h-14'} src={AzureImage} alt=""/>
+
+               </div>
+           </div>
        </div>
 
    </div>
