8ede32b61e4f0f245914d43ff31bdb9370dc3395
Maria de los Angeles Ramirez <mariaramireeez2002@gmail.com>
2025-07-15 13:41:54 -0400

    feat: responsive span linear

--- src/components/Molecules/CardsContainer/index.tsx
+++ src/components/Molecules/CardsContainer/index.tsx
@@ -2,6 +2,7 @@ import type {CardProps} from "../../Atoms/Card";
 import type {FC} from "react";
 import {CardSection} from "../CardSection";
 import {CustomImage, type CustomImageProps} from "../../Atoms/CustomImage";
+import {useIsMobile} from "../../../hooks/useIsMobile.tsx";
 
 export interface CardsContainerProps{
     cardsInfo: {
@@ -22,16 +23,18 @@ export interface CardsContainerProps{
 }
 
 export const CardsContainer:FC<CardsContainerProps> = ({cardsInfo}) => {
+    const {isMobile} = useIsMobile()
     return (
         <div className={'flex flex-wrap items-center justify-evenly py-10'}>
             {Object.entries(cardsInfo).map(([key, value]) => (
 
                <div key={key}>
                    <div className={'flex  items-center justify-center'}>
-                       <span className={'h-0.5 bg-gray-600 w-72'}></span>
+                       {!isMobile &&  <span className={'h-0.5 bg-gray-600 w-72'}></span>
+                       }
                        <CustomImage className={'h-40 mb-2'} {...value.imageSection.image}/>
-                       <span className={'h-0.5 bg-gray-600 w-72'}></span>
-
+                       {!isMobile &&  <span className={'h-0.5 bg-gray-600 w-72'}></span>
+                       }
 
                    </div>
 
