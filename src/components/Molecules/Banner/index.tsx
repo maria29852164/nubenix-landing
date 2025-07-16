65a443449ea06a5828c9ab847c9998192780dea3
Maria de los Angeles Ramirez <mariaramireeez2002@gmail.com>
2025-07-16 12:27:16 -0400

    feat: remove icons

--- src/components/Molecules/Banner/index.tsx
+++ src/components/Molecules/Banner/index.tsx
@@ -22,20 +22,12 @@ export const Banner:FC<BannerProps> = ({titleOne,titleTwo,description})=>{
            <h2 className={'lg:text-[60px]  md:text-[40px]   font-bold '}>{titleTwo}</h2>
 
            <p className={'text-lg max-w-[700px] pt-10'}>{description}</p>
-            <h2 className={'text-white'}>gholsa</h2>
            <div className={'flex flex-wrap items-center pt-6'}>
                <div className={'px-2 pt-4'}>
                    <img className={'lg:h-20 h-14'} src={AWSImage}/>
 
                </div>
-               <div className={'px-2 pt-2 '}>
-                   <img className={'lg:h-20 lg:w-72 h-14'} src={GoogleCloudImage} alt=""/>
-
-               </div>
-               <div className={'px-2 pt-2'}>
-                   <img className={'lg:h-20 lg:w-64 h-14'} src={AzureImage} alt=""/>
-
-               </div>
+      
            </div>
        </div>
 
