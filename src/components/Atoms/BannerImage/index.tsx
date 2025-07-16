4c7147c16e3d0bb5e6de185851168522095186e1
Maria de los Angeles Ramirez <mariaramireeez2002@gmail.com>
2025-07-16 09:46:34 -0400

    fix: black color

--- src/components/Atoms/BannerImage/index.tsx
+++ src/components/Atoms/BannerImage/index.tsx
@@ -1,6 +1,6 @@
 
-import Banner from '../../../../public/images/banner-home.png'
+import Banner from '../../../../public/images/banner.jpg'
 
 export const BannerImage = () => {
-    return <img src={Banner} className={'w-full h-[75vh]'} alt="BannerImage" />
+    return <img src={Banner} className={'w-full h-[75vh] grayscale-[60%] '} alt="BannerImage" />
 }
